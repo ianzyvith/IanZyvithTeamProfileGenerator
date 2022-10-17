2988f31c68d932f690ed17a827e7bfd18c57b295
Ian Zyvith <104524680+ianzyvith@users.noreply.github.com>
2022-10-17 09:20:07 -0400

    Update html-template.js

--- src/html-template.js
+++ src/html-template.js
@@ -10,7 +10,7 @@ const createManager = function (manager) {
             <div class="card-body">
                 <p class="id">ID: ${manager.id}</p>
                 <p class="email">Email: <a href="mailto:${manager.email}">${manager.email}</a></p>
-                <p class="office">Office Number: ${manager.officeNumber}</p>
+                <p class="office">Number: ${manager.number}</p>
             </div>
         </div>
     </div>
